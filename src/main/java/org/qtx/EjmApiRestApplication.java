82b87a01b1816364d468929223e9b25557cfc81f
Alex Cruz <acruz@qualitrain.com.mx>
2022-08-12 23:58:34 -0500

    Preparado para mvn site, site-deploy, deploy ¿y release?
    pom con configuracion de scm, distributionManagement, plugin de release
    pom con plugin de assembly comentado (necesario si no fuera Spring boot)
    Clase principal despliega la versión del empacado si se corre con java
    -jar

--- src/main/java/org/qtx/EjmApiRestApplication.java
+++ src/main/java/org/qtx/EjmApiRestApplication.java
@@ -1,12 +1,22 @@
 package org.qtx;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EjmApiRestApplication {
 
+	private static Logger bitacora = LoggerFactory.getLogger(EjmApiRestApplication.class);
+	
 	public static void main(String[] args) {
+		bitacora.info("Iniciando Aplicacion Version:" 
+	                  + EjmApiRestApplication.class
+	                                         .getPackage()
+	                                         .getImplementationVersion() );
+		// Al ejecutar mvn package se guardará en el jar la versión descrita en el pom.xml
+		// Al versión será visible al ejecutar el uber jar generado por spring-boot-maven-plugin, con java -jar
 		SpringApplication.run(EjmApiRestApplication.class, args);
 	}
 
